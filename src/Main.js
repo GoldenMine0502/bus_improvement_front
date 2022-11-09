f6cd4ee53a29a73f8657f936a08c9cd93d7fa708
GoldenMine0502 <ehe123@naver.com>
2022-11-09 19:33:42 +0900

    주석 제거

--- src/Main.js
+++ src/Main.js
@@ -22,17 +22,6 @@ function Main() {
         return new kakao.maps.Map(container, options)
     }
 
-    // const createMap = (markers) => {
-    //     const staticMapContainer = document.getElementById('map');
-    //     const staticMapOption = {
-    //         // 인천대학교 위도 경도: 37.3751° N, 126.6328° E
-    //         center: new kakao.maps.LatLng(INU_LATITUDE, INU_LONGITUDE),
-    //         level: 4,
-    //         marker: markers
-    //     };
-    //     return new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-    // }
-
     const registerDragStartEvent = (map) => {
         kakao.maps.event.addListener(map, 'dragstart', function(mouseEvent) {
             console.log("drag start, " + mapObjects.length)
@@ -194,87 +183,11 @@ function Main() {
                 // index++;
             }
         }
-        // usages
-        // console.log(usages.length);
-        // let index = 0;
-        //
-        // while(index < usages.length) {
-        //     const first = usages[index]
-        //
-        //     const list = [new kakao.maps.LatLng(first["posY"], first["posX"])]
-        //
-        //     while(index < usages.length) {
-        //         const middle = usages[index]
-        //
-        //         // console.log(index + ", " + first + ", " + middle["sequence"])
-        //         if(first["sequence"] === middle["sequence"]) {
-        //             list.push(new kakao.maps.LatLng(middle["posY"], middle["posX"]));
-        //             index++;
-        //         } else {
-        //             break;
-        //         }
-        //
-        //         // count++;
-        //         // if(count >= 1000)
-        //         //     break;
-        //     }
-        //
-        //     // 지금 리스트는 해당 sequence에 대한 연결 정보를 담고 있다.
-        //     const polyline = new kakao.maps.Polyline({
-        //         path: list, // 선을 구성하는 좌표배열 입니다
-        //         strokeWeight: 5, // 선의 두께 입니다
-        //         strokeColor: '#FFAE00', // 선의 색깔입니다
-        //         strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        //         strokeStyle: 'solid' // 선의 스타일입니다
-        //     });
-        //
-        //     mapObjects.push(polyline);
-        //     polyline.setMap(map);
-        //
-        //     index++;
-        // }
-
-        // fetch('http://localhost:8080/bus/station')
-        // fetch(`http://localhost:8080/bus/station?x=${lng}&y=${lat}&rangeX=${rangeX}&rangeY=${rangeY}`)
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         if(lastUpdate === currentUpdate) {
-        //             console.log(data)
-        //             data.forEach(it => {
-        //                 const result = proj4('TM127', 'WGS84', [it["posX"], it["posY"]]);
-        //                 const latitude = result[1]
-        //                 const longitude = result[0]
-        //
-        //                 // console.log(latitude, longitude)
-        //
-        //                 const circle = new kakao.maps.Circle({
-        //                     center: new kakao.maps.LatLng(latitude, longitude),  // 원의 중심좌표 입니다
-        //                     radius: 25, // 미터 단위의 원의 반지름입니다
-        //                     strokeWeight: 1, // 선의 두께입니다
-        //                     strokeColor: '#75B8FA', // 선의 색깔입니다
-        //                     strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        //                     strokeStyle: 'dashed', // 선의 스타일 입니다
-        //                     fillColor: '#CFE7FF', // 채우기 색깔입니다
-        //                     fillOpacity: 0.95  // 채우기 불투명도 입니다
-        //                 });
-        //
-        //                 mapObjects.push(circle)
-        //                 // 지도에 원을 표시합니다
-        //                 circle.setMap(map);
-        //             })
-        //
-        //             let k = data[0]["shortId"]
-        //         }
-        //     })
-        //     .catch((err) => {
-        //         console.log(err.message);
-        //     });
     }
 
     useEffect(() => {
         // https://gist.github.com/allieus/1180051/ab33229e820a5eb60f8c7971b8d1f1fc8f2cfabb
         // https://fascinate-zsoo.tistory.com/29
-        //
         proj4.defs('TM127', "+proj=tmerc +lat_0=38 +lon_0=127.0028902777777777776 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +towgs84=-146.43,507.89,681.46")
         proj4.defs('TM128', "+proj=tmerc +lat_0=38 +lon_0=128E +k=0.9999 +x_0=400000 +y_0=600000 +ellps=bessel +towgs84=-146.43,507.89,681.46")
         proj4.defs('GRS80', "+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs")
